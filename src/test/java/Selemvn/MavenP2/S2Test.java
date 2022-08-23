7d9c947f5b3767a4ef3a2977a7e49b319e07b189
DeepakDube <deepakdube3721@gmail.com>
2022-08-23 20:47:49 +0530

    first commit

---
+++ src/test/java/Selemvn/MavenP2/S2Test.java
@@ -0,0 +1,16 @@
+package Selemvn.MavenP2;
+
+import org.testng.annotations.Test;
+
+public class S2Test {
+	@Test
+	public void Browseratm2()
+	{
+		System.out.println("Browseratm2");
+	}
+	@Test
+	public void elementUi2()
+	{
+		System.out.println(" elementUi2");
+	}
+}
---
+++ src/test/java/Selemvn/MavenP2/STest.java
@@ -0,0 +1,19 @@
+package Selemvn.MavenP2;
+
+import org.testng.annotations.Test;
+
+public class STest {
+
+	@Test
+	public void Browseratm()
+	{
+		System.out.println("Browseratm");
+	}
+	@Test
+	public void elementUi()
+	{
+		System.out.println(" elementUi");
+	}
+}
+
+
